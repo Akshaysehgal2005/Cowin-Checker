41974f622199b2f02bcf16a3f2887e8413fef56a
Akshay Sehgal <akshaysehgal2005@gmail.com>
2021-05-09 06:10:36 +0530

    test

--- CowinCheckerApp/views.py
+++ CowinCheckerApp/views.py
@@ -10,7 +10,7 @@ def index(request):
 def bypin(request):
         pin = request.GET.get("pin",'122001')
         date = request.GET.get("date",'08-05-2021')
-
+        print(request)
         result = get_by_pin(pin, date)
         return JsonResponse(result)
 
