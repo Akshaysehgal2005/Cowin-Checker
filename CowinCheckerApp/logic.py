ecbcba28291d5d3ad313405942603138633cfa00
Akshay Sehgal <akshaysehgal2005@gmail.com>
2021-05-10 23:40:44 +0530

    header fix

--- CowinCheckerApp/logic.py
+++ CowinCheckerApp/logic.py
@@ -26,19 +26,7 @@ def get_by_pin(pin, date):
 
     url_pin_fill = url_pin + '?pincode='+ pin + '&date=' + date
 
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Origin': 'https://selfregistration.cowin.gov.in',
-        'Authorization': f'Bearer "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiI3ZTk0ZjVkNS0zMmI2LTRmZDItYjM0MC0xOGM4YzY5NTIxNzkiLCJ1c2VyX2lkIjoiN2U5NGY1ZDUtMzJiNi00ZmQyLWIzNDAtMThjOGM2OTUyMTc5IiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo5OTE2NTk0Nzc4LCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjMzNTE4NTY1MTk5MzYwLCJzZWNyZXRfa2V5IjoiYjVjYWIxNjctNzk3Ny00ZGYxLTgwMjctYTYzYWExNDRmMDRlIiwidWEiOiJNb3ppbGxhLzUuMCAoTWFjaW50b3NoOyBJbnRlbCBNYWMgT1MgWCAxMF8xNV83KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvOTAuMC40NDMwLjkzIFNhZmFyaS81MzcuMzYiLCJkYXRlX21vZGlmaWVkIjoiMjAyMS0wNS0wOVQwMToxNTozMC4xMjRaIiwiaWF0IjoxNjIwNTIyOTMwLCJleHAiOjE2MjA1MjM4MzB9.KvVMoL8BL1OszBbKzE-lHvsUNMHPCFps0_Wj266PdxA"',
-        'DNT': '1',
-        'Connection': 'keep-alive',
-        'Referer': 'https://selfregistration.cowin.gov.in/',
-        'Sec-GPC': '1',
-        'TE': 'Trailers',
-    }
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
     result = requests.get(url_pin_fill, headers=headers)
     print('response from api setu!!:', result)
